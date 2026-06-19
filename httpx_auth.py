f15faa7b084674932b3bd3694b513156d44d17e4
kirillsawinov2020-code <kirill.sawinov2020@gmail.com>
2026-06-19 18:19:22 +0300

    Автотест авторизации

---
+++ httpx_auth.py
@@ -0,0 +1,16 @@
+import httpx
+
+response = httpx.get("https://jsonplaceholder.typicode.com/todos/1")
+print(response.status_code)
+print(response.json())
+
+data = {
+    "title": "Какашка сладкая",
+    "completed": False,
+    "userId": 1
+}
+
+response = httpx.post("https://jsonplaceholder.typicode.com/todos/",json=data)
+
+print(response.status_code)
+print(response.json())
